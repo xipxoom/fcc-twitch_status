5f28cac97f1f386608d96075f892aa38b64da209
George Dhoore <georgiepoo@gmail.com>
2016-04-14 12:14:44 -0400

    Refactored previous commit to properly handle promises.

--- app/js/app.js
+++ app/js/app.js
@@ -8,39 +8,34 @@ $(function() {
 
 
 function updateStreams() {
-  var streamArr = fetchAllStreams();
-  displayStreams(streamArr);
-}
-
-function fetchAllStreams() {
+  // Clear out the stale data
+  $('#mainContainer').html("");
+  // And fetch the fresh
   var streamList = $('#streamList').val().split('\n');
-  var streamData = [];
   for (var i = 0, len = streamList.length; i < len; i++) {
-    streamData.push(fetchStreamData(streamList[i]));
+    fetchStreamData(streamList[i]);
   }
-  return streamData;
 }
 
 function fetchStreamData( streamName ) {
-  var obj = {};
   $.getJSON('https://api.twitch.tv/kraken/streams/' + streamName + '?callback=?', function(data){
-    obj.streamName = data.stream.channel.display_name;
-    obj.preview = data.stream.preview.medium;
-    obj.status = data.stream.channel.status;
-    obj.logo = data.stream.channel.logo;
-    obj.url = data.stream._links.self;
-    console.log(data.stream.channel.logo);
+    var streamObj = {};
+    streamObj.streamName = data.stream.channel.display_name;
+    streamObj.preview = data.stream.preview.medium;
+    streamObj.status = data.stream.channel.status;
+    streamObj.logo = data.stream.channel.logo;
+    streamObj.url = data.stream._links.self;
+
+    displayStream(streamObj);
   });
-  console.dir(obj.logo);
-  return obj;
 }
 
-function displayStreams( streamArr ) {
+function displayStream( streamObj ) {
   $('#mainContainer').append(
     '<div class="twitchContainer col-xs-12 col-sm-4">' +
-    '<img class="streamLogo" src="' + streamArr[0].logo + '" alt="logo">' +
-    '<h2>' + streamArr[0].streamName + '</h2>' +
-    '<p>' + streamArr[0].status + '</p>' +
+    '<img class="streamLogo" src="' + streamObj.logo + '" alt="logo">' +
+    '<h2>' + streamObj.streamName + '</h2>' +
+    '<p>' + streamObj.status + '</p>' +
     '</div>'
   );
 }
