6fb5f7a659831d34176c106ff74002aa93a84b91
George Dhoore <georgiepoo@gmail.com>
2016-04-15 12:35:05 -0400

    Begin handling stream that aren't broadcasting

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,7 @@ function updateStreams() {
   // Clear out the stale data
   $('#mainContainer').html("");
   // And fetch the fresh
+  // TODO: Remove blank lines
   var streamList = $('#streamList').val().split('\n');
   for (var i = 0, len = streamList.length; i < len; i++) {
     fetchStreamData(streamList[i]);
@@ -20,18 +21,45 @@ function updateStreams() {
 function fetchStreamData( streamName ) {
   $.getJSON('https://api.twitch.tv/kraken/streams/' + streamName + '?callback=?', function(data){
     var streamObj = {};
-    streamObj.streamName = data.stream.channel.display_name;
-    streamObj.preview = data.stream.preview.medium;
-    streamObj.status = data.stream.channel.status;
-    streamObj.logo = data.stream.channel.logo;
-    streamObj.url = data.stream.channel.url;
+    console.log(data);
+
+    // Account not streaming
+    if (!data.stream){
+      console.log('Not currently streaming.  Handle this!');
+
+    // Account doesn't exist
+    } else if (data.status == 404) {
+      streamObj.streamName = streamName;
+      streamObj.preview = './img/static.jpg';
+      streamObj.status = 'Account Doesn\'t exist';
+      streamObj.logo = './img/question.jpg';
+      streamObj.streaming = false;
+
+    // Account closed
+    } else if (data.status == 422) {
+      streamObj.streamName = streamName;
+      streamObj.preview = './img/static.jpg';
+      streamObj.status = 'Account Closed';
+      streamObj.logo = './img/question.jpg';
+      streamObj.streaming = false;
+
+    } else {
+    // Currently streaming
+      streamObj.streamName = data.stream.channel.display_name;
+      streamObj.preview = data.stream.preview.medium;
+      streamObj.status = data.stream.channel.status;
+      streamObj.logo = data.stream.channel.logo;
+      streamObj.url = data.stream.channel.url;
+      streamObj.streaming = true;
+    }
 
     displayStream(streamObj);
   });
 }
 
+
 function displayStream( streamObj ) {
-  $('<div class="twitchContainer col-xs-12 col-sm-4">' +
+  var container = $('<div class="twitchContainer col-xs-12 col-sm-4">' +
       '<img class="streamLogo" src="' + streamObj.logo + '" alt="logo">' +
       '<h2>' + streamObj.streamName + '</h2>' +
       '<p>' + streamObj.status + '</p>' +
@@ -40,6 +68,6 @@ function displayStream( streamObj ) {
   .css('background-image', 'url(' + streamObj.preview + ')')
   .on('click', function() {
     window.open(streamObj.url, '_blank');
-  })
-  .appendTo($('#mainContainer'));
+  });
+  container.appendTo($('#mainContainer'));
 }
